7827a515abf9d7ffa867fd733f059a092db9676c
Ricardo Barrios <rjbm29@gmail.com>
2025-05-27 11:23:57 -0500

    vite silence deprecation api & add primereact library components

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 // https://vitejs.dev/config/
+const legacyDeprecationApiSilence = {
+  silenceDeprecations: ['legacy-js-api'],
+  quietDeps: true,
+};
+
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -10,6 +15,12 @@ export default defineConfig({
   resolve: {
     alias: {
       '@sass': path.resolve(__dirname, './src/sass'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      sass: legacyDeprecationApiSilence,
+      scss: legacyDeprecationApiSilence,
     },
   },
 });
